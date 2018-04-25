acb6298fd6ab22353ad678b3b94a69c7186cc6d1
Valentin Bourdaud <bourdaudv@gmail.com>
2018-04-25 11:41:18 +0200

    CalculException passant et Assertj effectué

--- src/main/java/dev/service/CalculService.java
+++ src/main/java/dev/service/CalculService.java
@@ -3,18 +3,29 @@ package dev.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import dev.exception.CalculException;
+
 public class CalculService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(CalculService.class);
 
-	public int additionner(String expression){
-		
-		LOG.debug("Evaluation de l'expression" + expression);
-		String [] tab =  expression.split("\\+");
-		int somme =0 ;
-		for(int i=0;i<tab.length;i++){
+	public int additionner(String expression) {
 
-		somme += Integer.parseInt(tab[i]);}
+		LOG.debug("Evaluation de l'expression" + expression);
+
+		String[] tab = expression.split("\\+");
+
+		int somme = 0;
+		for (int i = 0; i < tab.length; i++) {
+
+			try {
+				somme += Integer.parseInt(tab[i]);
+			}
+
+			catch (Exception e) {
+				throw new CalculException();
+			}
+		}
 		return somme;
 
 	}
--- src/main/java/dev/service/CalculServiceTest.java
+++ src/main/java/dev/service/CalculServiceTest.java
@@ -1,10 +1,12 @@
 package dev.service;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import dev.exception.CalculException;
 
 public class CalculServiceTest {
 
@@ -20,11 +22,26 @@ public class CalculServiceTest {
 		
 		LOG.info("Lorsque j'évalue l'addition de l'expression 1+3+4"); 
 
-		int somme = cs.additionner("4");
+		int somme = cs.additionner("1+3+4");
 
 		LOG.info("Alors j'obtiens le résultat 8");
-		assertTrue(somme==8);
+		assertThat(somme).isEqualTo(8);
 
 	}
 	
+	@Test(expected= CalculException.class)
+	public void testAdditionnerInvalid() throws Exception {
+
+		LOG.info("Etant donné, une instance de la classe CalculService"); 
+		CalculService cs = new CalculService();
+		
+		LOG.info("Lorsque j'évalue l'addition de l'expression 1+3+4"); 
+
+		int somme = cs.additionner("1+3++4");
+
+		LOG.info("Alors j'obtiens le résultat 8");
+		assertThat(somme).isEqualTo(8);
+
+	
+}
 }
