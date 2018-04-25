c8d31bc1ff700375f28900fc3e639938fabbf0d5
Valentin Bourdaud <bourdaudv@gmail.com>
2018-04-25 10:08:54 +0200

    Creation de la classe CalculService + CalculServiceTest

---
+++ src/main/java/dev/service/CalculServiceTest.java
@@ -0,0 +1,27 @@
+package dev.service;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CalculServiceTest {
+
+	private static final Logger LOG = LoggerFactory.getLogger(CalculServiceTest.class);
+
+
+	@Test
+
+	public void testAdditionner() throws Exception {
+
+		LOG.info("Etant donné, une instance de la classe CalculService"); 
+
+
+		LOG.info("Lorsque j'évalue l'addition de l'expression 1+3+4"); 
+
+		int somme = 0;
+
+		LOG.info("Alors j'obtiens le résultat 8");
+		
+	}
+	
+}
